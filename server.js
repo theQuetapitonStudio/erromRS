69337d5cb83454514b1c54d4ea01909111b95790
The Quetapiton Dev <oamost123@gmail.com>
2026-01-02 18:04:36 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+
+const app = express();
+app.use(express.static("public"));
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (ws) => {
+    ws.on("message", (msg) => {
+        // broadcast geral
+        for (const client of wss.clients) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(msg.toString());
+            }
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log("Rodando na porta", PORT);
+});
